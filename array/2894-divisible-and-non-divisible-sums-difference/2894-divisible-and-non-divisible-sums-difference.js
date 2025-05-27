be1ce4bdd878ce15d3e190c7abc33437cfa740ec
tuan.nguyen <winqti@gmail>
2025-05-27 13:53:35 +0700

    feat: fun easy algo with js

---
+++ array/2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+ var differenceOfSums = function(n, m) {
+	let sum = 0
+	for (let i = 1; i <= n; i++) {
+		if (i % m !== 0) {
+			sum += i;
+		}
+	}
+
+	for (let i = 1; i <= n; i++) {
+		if (i % m === 0) {
+			sum -= i;
+		}
+	}
+    
+	return sum
+};
