f88876c37c25318bfdfdd28a97604e8dbb1553c4
Tuan Nguyen Van Quoc <winqti@gmail.com>
2025-06-22 10:03:13 +0700

    feat(856): score of parentheses

---
+++ stack/856_score_of_parentheses/856_score_of_parentheses.go
@@ -0,0 +1,38 @@
+package scoreofparentheses
+
+func scoreOfParentheses(s string) int {
+	stack := make([]byte, 0)
+	score := 0
+
+	nestedCount := 1
+	isNested := false
+	for i := range s {
+		// nested = 0 && len of stack = 1
+		if s[i] == '(' {
+			if len(stack) > 0 {
+				nestedCount *= 2
+				isNested = false
+			}
+			stack = append(stack, s[i])
+		} else {
+			top := stack[len(stack)-1]
+			if nestedCount == 1 && top == '(' {
+				if isNested == true {
+					isNested = false
+					stack = stack[:len(stack)-1]
+					continue
+				}
+				score += 1
+			} else if nestedCount > 1 && top == '(' {
+				if isNested != true {
+					score += nestedCount
+				}
+				nestedCount /= 2
+				isNested = true
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return score
+}
