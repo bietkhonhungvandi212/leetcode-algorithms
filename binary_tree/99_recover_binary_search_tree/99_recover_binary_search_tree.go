abbaa1f8b16067be212c432222757ab9b4a9258c
Tuan Nguyen Van Quoc <winqti@gmail.com>
2025-07-06 16:54:50 +0700

    feat 99

---
+++ binary_tree/99_recover_binary_search_tree/99_recover_binary_search_tree.go
@@ -0,0 +1,37 @@
+package recoverbinarysearchtree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func recoverTree(root *TreeNode) {
+	var pred, first, second *TreeNode
+
+	var dfs func(root *TreeNode)
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		dfs(root.Left)
+		if pred != nil && pred.Val > root.Val {
+			if first == nil {
+				first = pred
+				second = root
+			} else {
+				second = root
+			}
+
+		}
+
+		pred = root
+		dfs(root.Right)
+	}
+
+	if first != nil {
+		first.Val, second.Val = second.Val, first.Val
+	}
+
+}
