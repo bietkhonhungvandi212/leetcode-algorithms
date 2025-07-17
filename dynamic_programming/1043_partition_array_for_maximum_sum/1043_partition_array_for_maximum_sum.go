a96b0590f60ed7c1613a963c4711968777285df4
Tuan Nguyen Van Quoc <winqti@gmail.com>
2025-07-17 22:53:18 +0700

    feat 1043 top down

--- dynamic_programming/1043_partition_array_for_maximum_sum/1043_partition_array_for_maximum_sum.go
+++ dynamic_programming/1043_partition_array_for_maximum_sum/1043_partition_array_for_maximum_sum.go
@@ -1,21 +1,39 @@
 package partitionarrayformaximumsum
 
-func maxSumAfterPartitioning(arr []int, k int) int {
+import "math"
+
+func maxSumAfterPartitioning_TopDown(arr []int, k int) int {
 	n := len(arr)
-	dp := make([]int, n+1)
-
-	for i := 1; i <= n; i++ {
-		maxValue := arr[i-1]
-		dp[i] = dp[i-1] + maxValue
-
-		for j := 2; j <= min(i, k); j++ {
-			maxValue = max(maxValue, arr[i-j])
-			dp[i] = max(dp[i], dp[i-j]+maxValue*j)
-		}
-
+	dp := make([]int, n)
+	for i := range dp {
+		dp[i] = -1
 	}
 
-	return dp[n]
+	var maxSumPartition func(index int) int
+	maxSumPartition = func(index int) int {
+		if index < 0 {
+			return 0
+		}
+
+		if dp[index] != -1 {
+			return dp[index]
+		}
+
+		maxSum := math.MinInt
+		currMax := arr[index]
+		for i := range min(index+1, k) {
+			currMax = max(arr[index-i], currMax)
+			cost := currMax*(i+1) + maxSumPartition(index-i-1)
+			if maxSum < cost {
+				maxSum = cost
+			}
+		}
+
+		dp[index] = maxSum
+		return dp[index]
+	}
+
+	return maxSumPartition(n - 1)
 }
 
 func max(a int, b int) int {
