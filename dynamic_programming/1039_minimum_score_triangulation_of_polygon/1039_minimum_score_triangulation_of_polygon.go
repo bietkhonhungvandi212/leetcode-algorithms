28b57dd2366d2309c98bf4b5ddb36455e9cf937a
tuan.nguyen <winqti@gmail>
2025-08-20 23:05:14 +0700

    feat 1039

--- dynamic_programming/1039_minimum_score_triangulation_of_polygon/1039_minimum_score_triangulation_of_polygon.go
+++ dynamic_programming/1039_minimum_score_triangulation_of_polygon/1039_minimum_score_triangulation_of_polygon.go
@@ -2,6 +2,32 @@ package minimumscoretriangulationofpolygon
 
 import "math"
 
+func minScoreTriangulationV1(values []int) int {
+	n := len(values)
+	dp := make([][]int, n)
+
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+
+	for length := 2; length <= n; length++ { // 2
+		for i := 0; i < n-length; i++ { // 0 < (n-2) = 1 -> true
+			endVertex := i + length
+			dp[i][endVertex] = math.MaxInt       // dp[0][2]
+			for j := i + 1; j < endVertex; j++ { // j = 1; j <= 2
+				cost := values[i]*values[j]*values[endVertex] + dp[i][j] + dp[j][endVertex]
+				//1 *  2 * 3 = 6
+				if cost < dp[i][endVertex] {
+					dp[i][endVertex] = cost
+				}
+			}
+		}
+	}
+
+	return dp[0][n-1]
+
+}
+
 func minScoreTriangulation(values []int) int {
 	n := len(values)
 	dp := make([][]int, n)
