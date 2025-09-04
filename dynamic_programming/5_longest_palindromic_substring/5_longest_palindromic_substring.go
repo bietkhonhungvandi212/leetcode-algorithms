e852562f7461ddd504872ca36df4ae3e5a9a33a8
bietrokhonhungvandi <winqti@gmail.com>
2025-09-04 09:54:01 +0700

    feat 5

--- dynamic_programming/5_longest_palindromic_substring/5_longest_palindromic_substring.go
+++ dynamic_programming/5_longest_palindromic_substring/5_longest_palindromic_substring.go
@@ -1,5 +1,35 @@
 package longestpalindromicsubstring
 
+func longestPalindrome_dp(s string) string {
+	n := len(s)
+	dp := make([][]bool, n)
+
+	for i := range dp {
+		dp[i] = make([]bool, n)
+	}
+
+	max := 0
+	start := 0
+	end := 0
+
+	for i := range n {
+		dp[i][i] = true
+		for j := range i {
+			if s[i] == s[j] && (i-j <= 2 || dp[j+1][i-1] == true) {
+				dp[j][i] = true
+				length := i - j + 1
+				if length > max {
+					max = length
+					start = j
+					end = i
+				}
+			}
+		}
+	}
+
+	return s[start : end+1]
+}
+
 func longestPalindrome(s string) string {
 	left := 0
 	right := 0
